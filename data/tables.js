fca82ae5f99579cec4e515005cbb8486b0eeec46
Foti.Mougos <foti.mougos@gmail.com>
2020-04-16 19:55:27 +1000

    added table files and route files

---
+++ data/tables.js
@@ -0,0 +1,13 @@
+
+//List of reserved tables
+var tableList = [
+    {
+      name: "Ralph Wiggum",
+      email: "rwiggum@mydomain.com",
+      id: "rwiggum",
+      phone: "03 9999 9999"
+    }
+  ];
+  
+  module.exports = tableList;
+  
